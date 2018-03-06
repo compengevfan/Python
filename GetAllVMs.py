68356c9dbb00b55ad28a9bedfb21e71954aef604
compengevfan <chris.dupree@gmail.com>
2018-03-06 08:59:01 -0500

    first script

---
+++ GetAllVMs.py
@@ -0,0 +1,29 @@
+from pyVim.connect import SmartConnect
+from pyVmomi import vim
+import ssl
+import getpass
+ 
+# Get all the Vms from vCenter server inventory and print its name
+# Below is Python 2.7.x code, which can be easily converted to python 3.x version
+ 
+Username = raw_input("Gimme yo username: ")
+Password = getpass.getpass(prompt='Gimme yo password:')
+s=ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+s.verify_mode=ssl.CERT_NONE
+si= SmartConnect(host="iad-vc001.fanatics.corp", user=Username, pwd=Password,sslContext=s)
+content=si.content
+ 
+# Method that populates objects of type vimtype
+def get_all_objs(content, vimtype):
+        obj = {}
+        container = content.viewManager.CreateContainerView(content.rootFolder, vimtype, True)
+        for managed_object_ref in container.view:
+                obj.update({managed_object_ref: managed_object_ref.name})
+        return obj
+ 
+#Calling above method
+getAllVms=get_all_objs(content, [vim.VirtualMachine])
+ 
+#Iterating each vm object and printing its name
+for vm in getAllVms:
+        print vm.name
