b6494c1c1b8aa392a22bf3735d178e1e9b357b77
compengevfan <chris.dupree@gmail.com>
2019-03-20 20:43:11 -0400

    update

---
+++ Learn Python Programming Masterclass/2. Variables.py
@@ -0,0 +1,41 @@
+# greeting = "Bruce"
+# _myName = "Tim"
+# Tim45 = "Good" 
+# Tim_Was_57 = "Hello"
+# Greeting = "There"
+# print(Tim_Was_57 + ' ' + greeting)
+
+# age = 24
+# print(age)
+
+# #This doesn't work
+# #print(greeting + age)
+
+# a = 12
+# b = 3
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+
+# for i in range(1,a//b):
+#     print(i)
+
+# print(a + b / 3 - 4 * 12)
+# print(8/2*3)
+# print(8*3/12)
+
+# print((((a+b)/3)-4)*12)
+
+parrot = "Norwegian Blue"
+print (parrot)
+print(parrot[0])
+print(parrot[3])
+#print(parrot[14])
+print(parrot[-1])
+print(parrot[0:6])
+print(parrot[:6])
+print(parrot[6:])
+
